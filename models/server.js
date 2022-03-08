40bbfaf6d6557d416166373343aef43efff7d7d3
Omar Alvarado <omar777g@gmail.com>
2022-03-07 20:27:31 -0500

    Inicio de Proyecto

---
+++ models/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app)
+        this.io = require('socket.io')(this.server)
+
+        this.paths = {
+        }
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+        
+        //this.app.use( this.paths.auth, require('../routes/auth'));
+    }
+
+    listen() {
+        this.server.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
